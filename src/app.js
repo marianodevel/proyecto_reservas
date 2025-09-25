81f4e5d8622d739fdc56bf85c75487bf22463f96
marianodevel <marianodevel@pm.me>
2025-09-25 09:01:29 -0300

    agregada documentacion salones

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const serviciosRuta = require("./rutas/servicios.ruta");
 const turnosRuta = require("./rutas/turnos.ruta");
 const reservasRuta = require("./rutas/reservas.ruta");
 const usuariosRuta = require("./rutas/usuarios.ruta");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./src/docs/swagger.yaml");
 
 const app = express();
 app.use(express.json());
@@ -14,5 +17,8 @@ app.use("/servicios", serviciosRuta);
 app.use("/turnos", turnosRuta);
 app.use("/reservas", reservasRuta);
 app.use("/usuarios", usuariosRuta);
+
+// Documentación
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
